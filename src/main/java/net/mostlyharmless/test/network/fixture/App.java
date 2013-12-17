30c9e2e8e0ce8b8478b6634e8bd6046b0e9778a7
Brian Roach <roach@basho.com>
2013-12-17 14:27:41 -0700

    Setting up maven project

---
+++ src/main/java/net/mostlyharmless/test/network/fixture/App.java
@@ -0,0 +1,13 @@
+package net.mostlyharmless.test.network.fixture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
