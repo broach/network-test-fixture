100674464d0323f6d825baa85c4a62577dea95d9
Brian Roach <roach@basho.com>
2013-12-19 10:40:47 -0700

    App with main

--- src/main/java/net/mostlyharmless/test/network/fixture/App.java
+++ src/main/java/net/mostlyharmless/test/network/fixture/App.java
@@ -1,9 +1,16 @@
 package net.mostlyharmless.test.network.fixture;
 
+import java.io.IOException;
+
 public class App 
 {
-    public static void main( String[] args ) 
+    private final static int startingPort = 5000;
+    
+    public static void main( String[] args ) throws IOException, InterruptedException 
     {
-        System.out.println( "Hello World!" );
+        NetworkTestFixture fixture = new NetworkTestFixture(startingPort);
+        Thread t = new Thread(fixture);
+        t.start();
+        t.join();
     }
 }
